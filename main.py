259045aae99548e49324e6e17d1f4c86c1e08750
Adrian Henkel <henkeladrian@icloud.com>
2022-01-17 17:23:46 +0100

    Updates main.py to complete skeleton
    The complete logic should be done. TODO: Test this with pre-trained models
    
    Co-Authored-By: Arend Lis <55484713+lisiarend@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,42 +1,117 @@
 import argparse
 import hashlib
-
+from typing import List
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from io import StringIO
+import h5py
+import numpy as np
+import torch.nn as nn
+import torch
+from common.models import *
+from common.cait_models import CaiT
 
 
 ################# ArgumentParser #################
-
-parser = argparse.ArgumentParser(description='Class Attention Model')
-parser.add_argument('--fasta',type=str,
-                    help='path_to_fasta')
-parser.add_argument('--emb',type=str,
-                    help='path_to_embeddings')
-parser.add_argument('--output',type=str,
-                    help='path_to_output')
+parser = argparse.ArgumentParser(description="Class Attention Model")
+parser.add_argument("--fasta", type=str, help="path_to_fasta")
+parser.add_argument("--emb", type=str, help="path_to_embeddings")
+parser.add_argument("--output", type=str, help="path_to_output", default="./output.tsv")
+parser.add_argument(
+    "--model_type",
+    type=str,
+    default="CAIT",
+    choices=["CNN", "MLP", "CAIT"],
+    help="Model type. Default CAIT",
+)
 args = parser.parse_args()
 
-fasta_path = parser.fasta + ".fasta"
-h5_path = parser.emb + ".h5"
-output_path = parser.output if parser.output + "/output.tsv" is not None else "./output.tsv"
 
+################# Mappings for labels #################
+label_mappings = {"Glob_SP": 0, "Glob": 1, "TM_SP": 2, "TM": 3}
+reverse_label_mappings = {val: key for key, val in label_mappings.items()}
+
+
+################# Read Data #################
+def read_fasta(path: str) -> List[SeqRecord]:
+    file = open(path)
+
+    def record_for(id: str, seq: str) -> SeqRecord:
+        # put back together to feed into SeqIO
+        # alternative would be to manually construct SeqRecord here
+        record = SeqIO.read(StringIO(id + seq), "fasta")
+        return record
+
+    return [record_for(id, seq) for id, seq in zip(file, file)]
+
+
+def as_dict(record: SeqRecord) -> dict:
+    # we don't currently use the base sequence for training, but it's here for future use
+    seq = (
+        str(record.seq).strip().replace("X", "*")
+    )  # dnachisel doesn't like X as stop codons
+    hash = hashlib.md5(seq.encode("UTF-8")).hexdigest()
+    return {"protein": record.name, "seq": seq, "seq_hash": hash}
+
+
+def complete_dicts(emb_path: str, dicts: List[dict]) -> List[dict]:
+    h5 = h5py.File(emb_path)
+    for each_dict in dicts:
+        hash = each_dict["seq_hash"]
+
+        embedding = h5.get(hash)
+        embedding = np.array(embedding).astype(np.float32)
+        if np.isnan(embedding).any():
+            embedding = np.zeros((1, 1024)).astype(np.float32)
+
+        each_dict["embedding"] = embedding
+    return dicts
+
+
+def read_data(path_fasta: str, path_h5: str) -> List[dict]:
+    records = read_fasta(path_fasta)
+    all_dicts = [as_dict(record) for record in records]
+    all_dicts = complete_dicts(path_h5, all_dicts)
+    return all_dicts
 
 
 ################# Load Model #################
+def load_model(model_type="CAIT") -> list:
+    if model_type == "CAIT":
+        model = CaiT()
+    elif model_type == "MLP":
+        model = MLP()
+    elif model_type == "CNN":
+        model = CNN()
+
+    model.load_state_dict(torch.load(f"models/{model_type}.pt"))
+    model.eval()
+    return model
 
 
-
-
-
-################# Test Model #################
-
-
-
+################# Predict Data #################
+def predict(data: List[dict], model: nn.Module) -> dict:
+    with torch.no_grad():
+        result_predicted = dict()
+        for each_dict in data:
+            prediction_output = model(data["embedding"])
+            confidence = torch.max(prediction_output)
+            predicted = torch.argmax(prediction_output)
+            assert predicted in range(3), "Something went wrong with the prediction"
+            string_label = reverse_label_mappings[int(predicted)]
+            result_predicted[each_dict["protein"]] = [string_label, confidence]
+    return result_predicted
 
 
 ################# Write Output #################
+def parse_output(results: dict, output_path: str) -> None:
+    with open(output_path, "w") as file:
+        for protein, prediction in results.items():
+            file.write(f"{protein}\t{prediction[0]}\t{prediction[1]}\n")
 
-# <prot_id>\t<label>\t<confidence_score>
-# prot_id as given in h5 or fasta
-# label TM, TM+SP, G, G+SP
-# confidence score between 0 and 1
 
-
+if __name__ == "__main__":
+    prediction_data = read_data(path_fasta=args.fasta, path_h5=args.emb)
+    model = load_model(args.model_type)
+    output = predict(prediction_data, model)
+    parse_output(output, output_path=args.output)
