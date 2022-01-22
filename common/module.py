08da733405291416bb80b3b010448a6d9cd6e296
Finn Gaida <github@fga.pw>
2022-01-22 15:58:03 +0100

    Remove att map plotting code

--- common/module.py
+++ common/module.py
@@ -20,29 +20,6 @@ class CaitModule(LightningModule):
 
         if isinstance(self.model, CaiT):
             y_hat, attn_weights = self.model(embed)
-
-            # from https://www.kaggle.com/piantic/vision-transformer-vit-visualize-attention-map/notebook
-            render_attn_map = False
-            if render_attn_map:
-                att_mat = torch.stack(attn_weights).squeeze(1)
-                att_mat = torch.mean(att_mat, dim=1)
-
-                # To account for residual connections, we add an identity matrix to the
-                # attention matrix and re-normalize the weights.
-                residual_att = torch.eye(att_mat.size(1))
-                aug_att_mat = att_mat + residual_att
-                aug_att_mat = aug_att_mat / aug_att_mat.sum(dim=-1).unsqueeze(-1)
-
-                # Recursively multiply the weight matrices
-                joint_attentions = torch.zeros(aug_att_mat.size())
-                joint_attentions[0] = aug_att_mat[0]
-
-                for n in range(1, aug_att_mat.size(0)):
-                    joint_attentions[n] = torch.matmul(aug_att_mat[n], joint_attentions[n - 1])
-
-                v = joint_attentions[-1]
-                # TODO: render attn map
-
         else:
             y_hat = self.model(embed)
 
